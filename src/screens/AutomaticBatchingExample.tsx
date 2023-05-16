6e3cd904f8f0bb0ff8b46d26036467f1e3e4695d
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-05-15 17:35:27 -0700

    Add AutomaticBatching example

--- src/screens/AutomaticBatchingExample.tsx
+++ src/screens/AutomaticBatchingExample.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Button, Text, View} from 'react-native';
 
 /**
  * @see https://react.dev/blog/2022/03/08/react-18-upgrade-guide#automatic-batching
  * @see https://github.com/reactwg/react-18/discussions/21
  */
 export function AutomaticBatchingExampleScreen() {
+  const [count, setCount] = React.useState(0);
+  const [flag, setFlag] = React.useState(false);
+
+  function handleClick() {
+    setCount(c => c + 1);
+    setFlag(f => !f);
+  }
+
   return (
-    <>
-      <Text>Automatic Batching</Text>
-    </>
+    <View>
+      <Button title="Next" onPress={handleClick} />
+      <Text>Count: {count}</Text>
+      <Text>Flag: {`${flag}`}</Text>
+    </View>
   );
 }
